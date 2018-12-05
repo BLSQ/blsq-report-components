eac9a24084d0c830fb0fbdd25523f1bd8075ab6d
mestachs <mestachs>
2018-12-05 21:39:43 +0100

    Add bold and quantity test

--- src/components/shared/Cell.test.js
+++ src/components/shared/Cell.test.js
@@ -3,6 +3,15 @@ import Cell from "./Cell";
 import TestRenderer from "react-test-renderer";
 
 describe("Cell", () => {
+  const value = {
+    demo: {
+      code: "demo code",
+      value: 15564.524,
+      name: "super",
+      period: "2016Q3"
+    }
+  };
+
   it("is truthy", () => {
     expect(Cell).toBeTruthy();
   });
@@ -16,13 +25,16 @@ describe("Cell", () => {
   });
 
   it("renders amount with provided field", () => {
-    const value = {
-      demo: { code: "demo code", value: 15564.524, name: "super" }
-    };
     const tree = TestRenderer.create(
       <Cell variant="money" field="demo" value={value} />
     );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 
+  it("renders percentage with provided field", () => {
+    const tree = TestRenderer.create(
+      <Cell variant="percentage" field="demo" value={value} bold />
+    );
     expect(tree.toJSON()).toMatchSnapshot();
   });
 });

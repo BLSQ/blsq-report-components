eb55dc54a94332fc90f5f78241cd5971c6ec4685
butofleury <fleurybu@gmail.com>
2019-05-28 09:19:14 +0200

    Integrating signatures on example invoice

---
+++ example/src/invoices/Config.js
@@ -0,0 +1,7 @@
+const config = {
+  global: {
+    signatures: ["utyDVruoMgE", "Gj3WTTHhuSz"]
+  }
+};
+
+export default config;
--- example/src/invoices/demo-chc/Mapper.js
+++ example/src/invoices/demo-chc/Mapper.js
@@ -1,5 +1,13 @@
+import { Dhis2 } from "@blsq/blsq-report-components";
+import config from "../Config";
 class Mapper {
   mapValues(request, values) {
+    const dhis2 = new Dhis2();
+    const signatures= {
+        sign1: dhis2.getFileDataValue({de: config.global.signatures[0], ou:request.orgUnit.ancestors[1].id, pe: request.year}, dhis2.url),
+        sign2: dhis2.getFileDataValue({de: config.global.signatures[1], ou:request.orgUnit.ancestors[1].id, pe: request.year}, dhis2.url)
+      };
+
     const invoice = {
       orgUnit: request.orgUnit,
       orgUnits: request.orgUnits,
@@ -7,7 +15,8 @@ class Mapper {
       quarter: request.quarter,
       activities: [],
       total: {},
-      values: values.values
+      values: values.values,
+      signatures: signatures
     };
     return invoice;
   }

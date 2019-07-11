e66c1f274b2f2da2bd0ae7981d818481803a5cd0
butofleury <fleurybu@gmail.com>
2019-07-11 15:34:27 +0200

    Getting the current user infos

--- src/components/invoices/InvoiceContainer.js
+++ src/components/invoices/InvoiceContainer.js
@@ -107,6 +107,8 @@ class InvoiceContainer extends Component {
         invoiceType,
         this.props.invoices.mapper(invoiceTypeCode)
       );
+      invoice.currentUser = this.props.currentUser;
+
       const calculations = this.props.invoices.getOrbfCalculations(
         invoice,
         this.props.currentUser

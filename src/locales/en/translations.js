3a8a73e40600e54b9004347ef7231cbe818c1106
butofleury <fleurybu@gmail.com>
2019-08-12 09:21:10 +0200

    Translate org unit name label

--- src/locales/en/translations.js
+++ src/locales/en/translations.js
@@ -1,6 +1,7 @@
 const keys = {
   "report_and_invoices": "Report & Invoices",
   "app_name": "ORBF2 - Invoices & Reports",
-  "print": "Print"
+  "print": "Print",
+  "orgUnit_name": "Organisation Unit name"
 }
 export default keys

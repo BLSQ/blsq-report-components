d5879932106738128b6cf05c822c6777c00cb323
butofleury <fleurybu@gmail.com>
2019-08-13 18:01:15 +0200

    FIx periods links

--- src/components/invoices/InvoiceLink.js
+++ src/components/invoices/InvoiceLink.js
@@ -38,10 +38,10 @@ class InvoiceLink extends Component {
                 {invoiceType.name}
               </span>
 
-              {DatePeriods.split(quarterPeriod, invoiceType.frequency).map(subPeriod => (
-                  const period = DatePeriods.displayName(subPeriod, invoiceType.periodFormat || (invoiceType.frequency == "quarterly" ? "quarter" : invoiceType.frequency == "sixMonthly" ? "sixMonth" : "monthYear"));
-                  const splittedPeriod = period.split(" ");
-                  const linkPeriod = this.linkTo(invoiceType, subPeriod);
+              {DatePeriods.split(quarterPeriod, invoiceType.frequency).map((subPeriod) => (
+                  const period = DatePeriods.displayName(subPeriod, invoiceType.periodFormat || (invoiceType.frequency == "quarterly" ? "quarter" : invoiceType.frequency == "sixMonthly" ? "sixMonth" : "monthYear"))
+                  const splittedPeriod = period.split(" ")
+                  const linkPeriod = this.linkTo(invoiceType, subPeriod)
                   const translatedPeriod = this.translatePeriod(splittedPeriod[0]) + " " + splittedPeriod[1];
 
                   return (<Button

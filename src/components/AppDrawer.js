6a03f97d94cb8c871a50ca582d1f92cbf3d3a091
butofleury <fleurybu@gmail.com>
2019-09-04 13:23:34 +0200

    Back to previous them spacing style

--- src/components/AppDrawer.js
+++ src/components/AppDrawer.js
@@ -90,7 +90,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+    padding: theme.spacing.unit * 3,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen

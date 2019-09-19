4a773b3fb3ceeaedddb290a1e65ea319e322f42e
butofleury <fleurybu@gmail.com>
2019-09-19 10:42:34 +0200

    Clean code by using button invoice link

--- src/components/invoices/InvoiceLink.js
+++ src/components/invoices/InvoiceLink.js
@@ -5,7 +5,7 @@ import DatePeriods from "../../support/DatePeriods";
 
 
 function InvoiceLink (orgUnit, invoices, period) {
-   
+
     const codes = invoices.getInvoiceTypeCodes(orgUnit);
 
     if (codes === undefined || codes.length === 0) {
@@ -16,8 +16,8 @@ function InvoiceLink (orgUnit, invoices, period) {
 
     const quarterPeriod = DatePeriods.split(period, "quarterly")[0];
 
-    return invoiceTypes.map(invoiceType => 
-      {invoiceName:  invoiceType.name, 
+    return invoiceTypes.map(invoiceType =>
+      {invoiceName:  invoiceType.name,
        invoiceLinks:  DatePeriods.split(quarterPeriod, invoiceType.frequency).map(
             subPeriod => (
               <Button
@@ -40,97 +40,9 @@ function InvoiceLink (orgUnit, invoices, period) {
                 )}
               </Button>
             )
-          ); 
+          );
       }
       );
 }
 
 export default InvoiceLink;
-
-
-/*import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import { withStyles } from "@material-ui/core/styles";
-
-import DatePeriods from "../../support/DatePeriods";
-
-const styles = theme => ({
-  buttonLike: {
-    ...theme.typography.button,
-    backgroundColor: theme.palette.common.white,
-    padding: theme.spacing(1),
-    fontSize: "0.8125rem"
-  }
-});
-
-class InvoiceLink extends Component {
-  render() {
-    const orgUnit = this.props.orgUnit;
-
-    const codes = this.props.invoices.getInvoiceTypeCodes(orgUnit);
-
-    if (codes === undefined || codes.length === 0) {
-      return null;
-    }
-
-    const invoiceTypes = this.props.invoices.getInvoiceTypes(codes);
-
-    const quarterPeriod = DatePeriods.split(this.props.period, "quarterly")[0];
-
-    return invoiceTypes.map(invoiceType => (
-      <React.Fragment key={this.linkTo(invoiceType)}>
-        <React.Fragment>
-          <span className={this.props.classes.buttonLike}>
-            {invoiceType.name}
-          </span>
-
-          {DatePeriods.split(quarterPeriod, invoiceType.frequency).map(
-            subPeriod => (
-              <Button
-                key={invoiceType.code + "-" + subPeriod + "-" + orgUnit.id}
-                variant="text"
-                color="primary"
-                size="small"
-                component={Link}
-                to={this.linkTo(invoiceType, subPeriod)}
-                title={subPeriod}
-              >
-                {DatePeriods.displayName(
-                  subPeriod,
-                  invoiceType.periodFormat ||
-                    (invoiceType.frequency == "quarterly"
-                      ? "quarter"
-                      : invoiceType.frequency == "sixMonthly"
-                      ? "sixMonth"
-                      : "monthYear")
-                )}
-              </Button>
-            )
-          )}
-        </React.Fragment>
-
-        <br />
-      </React.Fragment>
-    ));
-  }
-
-  linkTo(invoiceType, period) {
-    return (
-      "/invoices/" +
-      period +
-      "/" +
-      this.props.orgUnit.id +
-      "/" +
-      invoiceType.code
-    );
-  }
-}
-
-InvoiceLink.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(InvoiceLink);
-*/

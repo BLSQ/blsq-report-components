5e20bc96e6faee034d6ce2cfcc1d86f0f2beee7c
Stéphan Mestach <smestach@bluesquare.org>
2020-12-17 13:46:32 +0100

    Remove sample app

--- example/src/invoices/Config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  global: {
-    signatures: ["utyDVruoMgE", "Gj3WTTHhuSz"],
-    validation: {dataSet: "l0YmmflzpC0", dataElement:["b5NXrf9IuSn", "ZdsBDg0Uu7A"]}
-  }
-};
-
-export default config;

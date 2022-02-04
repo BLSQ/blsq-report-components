b96a369fd9822f9529d54f2267163858c20f22ae
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-02-04 16:47:25 +0100

    feat: extracts contract object building into its own method

--- src/components/syncs/datasets/fetchDataSets.js
+++ src/components/syncs/datasets/fetchDataSets.js
@@ -1,26 +1,8 @@
 import PluginRegistry from "../../core/PluginRegistry";
 import _ from "lodash";
 
-export const fetchDataSets = async (allDataEntries, period) => {
-  const dhis2 = PluginRegistry.extension("core.dhis2");
+const computeContractsByDataEntryCode = (allDataEntries, activeContracts, dataSetsById, period) => {
   const project = PluginRegistry.extension("hesabu.project");
-  const api = await dhis2.api();
-  const dataElements = _.keyBy(
-    (await api.get("dataElements", { paging: false, fields: "id,name" })).dataElements,
-    (de) => de.id,
-  );
-  const dataSetIds = allDataEntries.flatMap((d) => (d.dataSetId ? [d.dataSetId] : d.dataSetIds));
-  const ds = await api.get("dataSets", {
-    filter: ["id:in:[" + dataSetIds.join(",") + "]"],
-    paging: false,
-    fields: ":all,dataSetElements[dataElement[id,name]]organisationUnits[id,name],workflow[:all]",
-  });
-  const dataSetsById = _.keyBy(ds.dataSets, (d) => d.id);
-
-  const contractService = PluginRegistry.extension("contracts.service");
-  const contracts = await contractService.findAll();
-
-  const activeContracts = contracts.filter((contract) => contract.matchPeriod(period));
   const contractsByDataEntryCode = {};
   for (let dataEntry of allDataEntries) {
     const dataEntryContracts = activeContracts.filter((activeContract) =>
@@ -33,7 +15,6 @@ export const fetchDataSets = async (allDataEntries, period) => {
       : [];
     const results = [];
     for (let dataSet of dataSets) {
-      contractsByDataEntryCode[dataEntry.code];
       const dataSetOrgunits = new Set(dataSet.organisationUnits.map((ou) => ou.id));
       const missingOrgunits = dataEntryContracts.map((c) => c.orgUnit).filter((ou) => !dataSetOrgunits.has(ou.id));
 
@@ -64,6 +45,34 @@ export const fetchDataSets = async (allDataEntries, period) => {
     }
     contractsByDataEntryCode[dataEntry.code] = results;
   }
+  return contractsByDataEntryCode;
+};
+
+export const fetchDataSets = async (allDataEntries, period) => {
+  const dhis2 = PluginRegistry.extension("core.dhis2");
+  const api = await dhis2.api();
+  const dataElements = _.keyBy(
+    (await api.get("dataElements", { paging: false, fields: "id,name" })).dataElements,
+    (de) => de.id,
+  );
+  const dataSetIds = allDataEntries.flatMap((d) => (d.dataSetId ? [d.dataSetId] : d.dataSetIds));
+  const ds = await api.get("dataSets", {
+    filter: ["id:in:[" + dataSetIds.join(",") + "]"],
+    paging: false,
+    fields: ":all,dataSetElements[dataElement[id,name]]organisationUnits[id,name],workflow[:all]",
+  });
+  const dataSetsById = _.keyBy(ds.dataSets, (d) => d.id);
+
+  const contractService = PluginRegistry.extension("contracts.service");
+  const contracts = await contractService.findAll();
+
+  const activeContracts = contracts.filter((contract) => contract.matchPeriod(period));
+  const contractsByDataEntryCode = computeContractsByDataEntryCode(
+    allDataEntries,
+    activeContracts,
+    dataSetsById,
+    period,
+  );
   return {
     contractsByDataEntryCode,
     dataElementsById: dataElements,

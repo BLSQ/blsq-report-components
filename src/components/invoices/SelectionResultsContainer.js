6ba831fd6259763062d3eb5dd10d316accfab6d6
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-02-14 11:59:20 +0100

    feat: filters invoices to limit display of org unit pyramid

--- src/components/invoices/SelectionResultsContainer.js
+++ src/components/invoices/SelectionResultsContainer.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Box from "@material-ui/core/Box";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import PluginRegistry from "../core/PluginRegistry";
 import PropTypes from "prop-types";
 import InvoiceLinks from "./InvoiceLinks";
 import Tooltip from "@material-ui/core/Tooltip";
@@ -21,77 +22,68 @@ const contractsToTooltip = (orgUnit, period) => {
   }
   return orgUnit.contracts.map((c, index) => (
     <div key={index}>
-      {c.codes.join(",") +
-        " : " +
-        c.startPeriod +
-        " " +
-        c.endPeriod +
-        " " +
-        (c.matchPeriod(period) ? "**" : "")}
+      {c.codes.join(",") + " : " + c.startPeriod + " " + c.endPeriod + " " + (c.matchPeriod(period) ? "**" : "")}
     </div>
   ));
 };
 
-class SelectionResultsContainer extends Component {
-  render() {
-    const { classes, t } = this.props;
-    return (
-      <React.Fragment>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{this.props.levels.slice(1).join(" > ")}</TableCell>
-              <TableCell>{this.props.t("name")}</TableCell>
-              <TableCell>{this.props.t("invoice")}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.props.orgUnits &&
-              this.props.orgUnits.map((orgUnit, index) => (
-                <TableRow key={orgUnit.id + index}>
-
-                  <TableCell >
-                    <Box >
-                      <Typography>
-                        {orgUnit.ancestors
-                          .slice(1)
-                          .map((a) => a.name)
-                          .join(" > ")}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    <Tooltip
-                      title={
-                        <div>
-                          {orgUnit.id} -
-                          {orgUnit.organisationUnitGroups
-                            .map((g) => g.name)
-                            .join(", ")}
-                          <br></br>
-                          {contractsToTooltip(orgUnit, this.props.period)}
-                        </div>
-                      }
-                    >
-                      <Box fontWeight="fontWeightBold">{orgUnit.name}</Box>
-                    </Tooltip>
-                  </TableCell>
-                  <TableCell>
-                    <InvoiceLinks
-                      orgUnit={orgUnit}
-                      hideCurrentInvoice={false}
-                      {...this.props}
-                    />
-                    <ExtensionsComponent extensionKey="invoices.selectionLinks" orgUnit={orgUnit} {...this.props} />
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </React.Fragment>
-    );
-  }
-}
+const SelectionResultsContainer = (props) => {
+  const { classes, t, orgUnits, levels, period } = props;
+  const invoices = PluginRegistry.extension("invoices.invoices");
+  const filteredOrgunits =
+    orgUnits &&
+    orgUnits.filter((ou) => {
+      const codes = invoices.getInvoiceTypeCodes(ou, period);
+      return codes && codes.length > 0;
+    });
+  return (
+    <React.Fragment>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>{levels.slice(1).join(" > ")}</TableCell>
+            <TableCell>{t("name")}</TableCell>
+            <TableCell>{t("invoice")}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredOrgunits &&
+            filteredOrgunits.map((orgUnit, index) => (
+              <TableRow key={orgUnit.id + index}>
+                <TableCell>
+                  <Box>
+                    <Typography>
+                      {orgUnit.ancestors
+                        .slice(1)
+                        .map((a) => a.name)
+                        .join(" > ")}
+                    </Typography>
+                  </Box>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <Tooltip
+                    title={
+                      <div>
+                        {orgUnit.id} -{orgUnit.organisationUnitGroups.map((g) => g.name).join(", ")}
+                        <br />
+                        {contractsToTooltip(orgUnit, period)}
+                      </div>
+                    }
+                  >
+                    <Box fontWeight="fontWeightBold">{orgUnit.name}</Box>
+                  </Tooltip>
+                </TableCell>
+                <TableCell>
+                  <InvoiceLinks orgUnit={orgUnit} hideCurrentInvoice={false} {...props} />
+                  <ExtensionsComponent extensionKey="invoices.selectionLinks" orgUnit={orgUnit} {...props} />
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </React.Fragment>
+  );
+};
 
 SelectionResultsContainer.propTypes = {
   classes: PropTypes.object.isRequired,

1208b7e5f7c3d5e04c9cbba2a8d4235cf61ed99e
Stéphan Mestach <smestach@bluesquare.org>
2022-06-17 08:51:33 +0200

    Remove periodFormat props use the config

--- src/components/dataentry/DataEntriesSection.js
+++ src/components/dataentry/DataEntriesSection.js
@@ -3,8 +3,10 @@ import PluginRegistry from "../core/PluginRegistry";
 import OrgunitRelatedSection from "../shared/OrgunitRelatedSection";
 import DataEntryLinks from "../shared/data_entries/DataEntryLinks";
 
-const DataEntriesSection = ({ orgUnit, period, dataEntryCode, periodFormat, orgUnitSectionStyle }) => {
+const DataEntriesSection = ({ orgUnit, period, dataEntryCode, orgUnitSectionStyle }) => {
   const dataEntryRegistry = PluginRegistry.extension("dataentry.dataEntries");
+  const config = PluginRegistry.extension("core.config");
+  
   let dataEntries = [];
   if (orgUnit && orgUnit.activeContracts && orgUnit.activeContracts[0]) {
     const expectedDataEntries = dataEntryRegistry.getExpectedDataEntries(orgUnit.activeContracts[0], period);
@@ -20,7 +22,7 @@ const DataEntriesSection = ({ orgUnit, period, dataEntryCode, periodFormat, orgU
             dataEntryCode={dataEntryCode || undefined}
             period={period}
             orgUnit={orgUnit}
-            periodFormat={periodFormat}
+            periodFormat={config.global.periodFormat}
           />
         )}
       </div>

016384a615c40f71b7199b6df3f68ab1dc2af2bf
Stéphan Mestach <smestach@bluesquare.org>
2023-10-27 15:26:29 +0200

    Allow to overide the iaso url from plugin config or

--- src/components/dataentry/EditIasoFormButton.js
+++ src/components/dataentry/EditIasoFormButton.js
@@ -3,20 +3,27 @@ import React, { useContext } from "react";
 import FormDataContext from "./FormDataContext";
 import { Button } from "@material-ui/core";
 import LoadingSpinner from "../shared/LoadingSpinner";
+import PluginRegistry from "../core/PluginRegistry";
 
-const EditIasoFormButton = ({ formId, currentUserId, period, orgUnitId, iasoToken, dataSetId }) => {
+const EditIasoFormButton = ({ formId, currentUserId, period, orgUnitId, iasoToken, dataSetId, iasoUrl }) => {
   const formDataContext = useContext(FormDataContext);
   const currentDataSetId = dataSetId ? dataSetId : formDataContext.dataSet.id;
   const [loading, setLoading] = React.useState(false);
-  const iasoHost = "https://iaso.bluesquare.org";
-  const iasoUrl = `${iasoHost}/api/enketo/public_create_url/`;
+
+  let iasoHost = iasoUrl || "https://iaso.bluesquare.org";
+  let iasoConfig = PluginRegistry.extension("iaso.config");
+  if (iasoConfig && iasoConfig.url) {
+    iasoHost = iasoConfig.url;
+  }
+
+  const iasoEnketoUrl = `${iasoHost}/api/enketo/public_create_url/`;
   const returnUrl = encodeURIComponent(window.location.href);
 
   const handleClick = async () => {
     setLoading(true);
     try {
       const resp = await fetch(
-        `${iasoUrl}?token=${iasoToken}&form_id=${formId}&period=${period}&external_user_id=${currentUserId}&external_org_unit_id=${orgUnitId}&to_export=true&return_url=${returnUrl}`,
+        `${iasoEnketoUrl}?token=${iasoToken}&form_id=${formId}&period=${period}&external_user_id=${currentUserId}&external_org_unit_id=${orgUnitId}&to_export=true&return_url=${returnUrl}`,
       ).then((r) => r.json());
       if (resp.url) {
         window.location.href = resp.url;
@@ -32,16 +39,14 @@ const EditIasoFormButton = ({ formId, currentUserId, period, orgUnitId, iasoToke
 
   const writable = formDataContext.isDataWritable(currentDataSetId);
 
-
   return (
     <Button
       color="secondary"
-      title={"Edit in iaso : " + formId}
-     
+      title={"Edit in iaso : " + formId + "\n" + iasoHost}
       onClick={handleClick}
       disabled={loading || !writable}
     >
-      {loading ? <LoadingSpinner></LoadingSpinner>  : "Edit"}
+      {loading ? <LoadingSpinner></LoadingSpinner> : "Edit"}
     </Button>
   );
 };
